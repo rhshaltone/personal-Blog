7c59e7d2999600fb7f91c7bf5e0e5c770edaacb0
Shaltone Rhimba <rhimbashaltone@gmail.com>
2025-08-02 13:37:15 +0300

    modified server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the root directory
 app.use(express.static(path.join(__dirname, "..")));
 
 // Blog posts API
